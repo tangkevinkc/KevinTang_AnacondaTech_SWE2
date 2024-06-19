1f7dea670e137ce5d487fa7932e7cbe0b40ebb99
tangkevinkc <tangkevinkc@gmail.com>
2024-06-18 17:03:21 -0700

    test

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+void readFile(string filename, void *data){
+    // read directory and place data into void pointer
+}
+
+void createFile(string filename, void *data){
+    // create common, a_only, and b_only files
+    // write to files after going through directories
+    // write from void *data to filename
+}
+
+void compareFiles(string file1, string file2){
+    // read both files, place into 2 void *data pointers
+    // compare both datasets
+    // create common file first, then a_only, then b_only
+}
+
+int main(){
+    // call necessary functions and tests for functions
+    // create executable
+    return 0;
+}
