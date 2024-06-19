e1c40a1693d9ba0d179d778c5840c45a8d68d73a
tangkevinkc <tangkevinkc@gmail.com>
2024-06-18 17:30:33 -0700

    v1

--- main.cpp
+++ main.cpp
@@ -3,20 +3,23 @@
 
 using namespace std;
 
-void readFile(string filename, void *data){
+// assume directory trees can have up to 100,000 files
+
+void readDirect(string filename, void *data){
     // read directory and place data into void pointer
 }
 
-void createFile(string filename, void *data){
+void writeFile(string filename, void *data){
     // create common, a_only, and b_only files
-    // write to files after going through directories
+    // write to files while going through directories
     // write from void *data to filename
 }
 
-void compareFiles(string file1, string file2){
-    // read both files, place into 2 void *data pointers
-    // compare both datasets
-    // create common file first, then a_only, then b_only
+void compareDirect(string file1, string file2){
+    // read both directories, place into 2 void *data pointers
+    // create common, a_only, and b_only files
+    // compare both pointers and write to correlating files as we read the data
+    // when reaching end of a file in a directory, automatically switch to new file 
 }
 
 int main(){
