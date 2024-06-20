766b93882a8f9c75988a5b84a2ca89bcb71e1b16
tangkevinkc <tangkevinkc@gmail.com>
2024-06-19 23:34:02 -0700

    v2

--- main.cpp
+++ main.cpp
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 // assume directory trees can have up to 100,000 files
+// 1. get file names from directories and place into vector<string>direct1, direct2
+// 2. generate hash value for each file from direct1 and place into unordered_map<size_t, filename>D1_hash
+// 3. iterate through direct2 and generate hash value for each file and D1_hash.find() to see if it exists
+// 4. based on the return statement determine if file is placed into shared or direct2_only file
+//    if it is shared make sure to remove from unordered_map
+// 5. after finishing direct2, all remaining elements in D1_hash should be direct1_only file
+// 6. iterate through D1_hash and get the key of each index and place into direct1_only file
 
-void readDirect(string filename, void *data){
-    // read directory and place data into void pointer
+bool getFiles(string direct, vector<string> &fileNames){
+    // pull file names from directory and place into vector
+    // return false if directory is invalid and return true if operation is done
+
+    return false;
 }
 
-void writeFile(string filename, void *data){
-    // create common, a_only, and b_only files
-    // write to files while going through directories
-    // write from void *data to filename
-}
 
-void compareDirect(string file1, string file2){
-    // read both directories, place into 2 void *data pointers
-    // create common, a_only, and b_only files
-    // compare both pointers and write to correlating files as we read the data
-    // when reaching end of a file in a directory, automatically switch to new file 
-}
 
-int main(){
+int main(){ 
     // call necessary functions and tests for functions
     // create executable
     return 0;
